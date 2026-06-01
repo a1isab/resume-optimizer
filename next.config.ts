81360032d35599358751c6844de6688b9e7510e6
a1isab <heyamer123@gmail.com>
2026-06-01 09:24:38 +0300

    fixes

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
-  output: 'export', 
-  basePath: isProd ? '/resume-optimizer' : '', // <-- Update this to your new repo name
-  images: {
-    unoptimized: true, 
-  },
+  /* You can delete output: 'export' and basePath. 
+     Vercel handles everything automatically from the root domain! */
 };
 
 export default nextConfig;
