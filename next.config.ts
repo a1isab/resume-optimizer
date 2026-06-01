be5fa21cb03c4bd17dfb2ff0dfc3dfa4bf2bf360
a1isab <heyamer123@gmail.com>
2026-06-01 09:24:03 +0300

    fixes

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,10 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  output: 'export', // Tells Next.js to build static HTML/CSS/JS files
-  basePath: isProd ? '/JobAssistant' : '', // Tells Next.js your repository name
+  output: 'export', 
+  basePath: isProd ? '/resume-optimizer' : '', // <-- Update this to your new repo name
   images: {
-    unoptimized: true, // GitHub Pages doesn't support Next.js dynamic image optimization
+    unoptimized: true, 
   },
 };
 
